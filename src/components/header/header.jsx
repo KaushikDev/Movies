b3848c81919f1ef3166c51fc5f72298bb5c07534
kaushik3pg <piyush.kaushik@3pillarglobal.com>
2024-10-16 12:52:23 +0530

    highglighted selected menu item

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,27 +1,47 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { MenuItems } from "./../../common/constants/menu";
 import "./header.scss";
-const Header = () => (
-  <header className="globalHeader">
-    <div className="logo-container">
-      <Link className="logo-link" to="/">
-        <i className="fa-solid fa-compact-disc"></i>
-      </Link>
-    </div>
-    <div className="menu-container">
-      <ul className="global-menu">
-        {MenuItems &&
-          MenuItems.map((item, id) => (
-            <li key={item + id} className="menu-items">
-              <Link className="menu-item" to={item.path}>
-                {item.title}
-              </Link>
-            </li>
-          ))}
-      </ul>
-    </div>
-  </header>
-);
+const Header = () => {
+  const { pathname } = useLocation();
+  const highlightSelectedMenuItem = (
+    currPath,
+    pagePath,
+    normalClass,
+    highlightedClass
+  ) => {
+    return currPath === pagePath ? highlightedClass : normalClass;
+  };
+
+  return (
+    <header className="globalHeader">
+      <div className="logo-container">
+        <Link className="logo-link" to="/">
+          <i className="fa-solid fa-compact-disc"></i>
+        </Link>
+      </div>
+      <div className="menu-container">
+        <ul className="global-menu">
+          {MenuItems &&
+            MenuItems.map((item, id) => (
+              <li key={item + id} className="menuItems">
+                <Link
+                  className={highlightSelectedMenuItem(
+                    pathname,
+                    item.path,
+                    "menuItem",
+                    "menuItemSelected"
+                  )}
+                  to={item.path}
+                >
+                  {item.title}
+                </Link>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
